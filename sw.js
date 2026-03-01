07ef20fc5201a692777d249bd0a155bb5234bb34
Gaugau <gauthier.gache@gmail.com>
2026-03-01 18:17:51 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-self.addEventListener("fetch", event => {
-  if (event.request.url.includes("script.google.com")) {
-    return;
-  }
-});
 self.addEventListener("install", () => {
   console.log("Service Worker installé");
 });
 
 self.addEventListener("fetch", event => {
+  // Si c'est une requête vers Google Script, on ne fait rien (on laisse le navigateur gérer)
+  if (event.request.url.includes("script.google.com")) {
+    return;
+  }
+  // Pour le reste du site, on fait le relais classique
   event.respondWith(fetch(event.request));
 });
